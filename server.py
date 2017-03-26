319ff4b226df2a17c31c7c116b5d8238220eeac8
Antonio <antonio.arena@kaust.edu.sa>
2017-03-26 16:21:53 +0300

    Logging options.

--- server.py
+++ server.py
@@ -1,4 +1,63 @@
-import cherrypy, os.path, addlog
+import logging
+import logging.config
+import cherrypy
+import os.path
+import addlog
+
+LOG_CONF = {
+    'version': 1,
+
+    'formatters': {
+        'void': {
+            'format': ''
+        },
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard',
+            'stream': 'ext://sys.stdout'
+        },
+        'cherrypy_access': {
+            'level':'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'void',
+            'filename': '/var/tmp/access.log',
+            'maxBytes': 10485760,
+            'backupCount': 20,
+            'encoding': 'utf8'
+        },
+        'cherrypy_error': {
+            'level':'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'void',
+            'filename': '/var/tmp/errors.log',
+            'maxBytes': 10485760,
+            'backupCount': 20,
+            'encoding': 'utf8'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO'
+        },
+        'cherrypy.access': {
+            'handlers': ['cherrypy_access'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'cherrypy.error': {
+            'handlers': [cherrypy_error'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    }
+}
 
 @cherrypy.popargs('mode', 'name', 'path', 'hostname', 'id')
 class LogServer:
@@ -10,6 +69,9 @@ class LogServer:
         except Exception:
             cherrypy.log("Error inserting data", traceback=True)
 
-
-conf = os.path.join(os.path.dirname(__file__), 'server.conf')
-cherrypy.quickstart(LogServer(), '/logs/', config=conf)
+if __name__ == '__main__':
+    conf = os.path.join(os.path.dirname(__file__), 'server.conf')
+    cherrypy.config.update(conf)
+    cherrypy.engine.unsubscribe('graceful', cherrypy.log.reopen_files)
+    logging.config.dictConfig(LOG_CONF)
+    cherrypy.quickstart(LogServer(), '/logs/')
