d5bca70b28653996a2f688ca9941587f172dfd03
Antonio Arena <arena.antonio@gmail.com>
2017-04-12 21:13:50 +0300

    Let's be sure we put OS specific modules before the genaral ones.

--- local.sh
+++ local.sh
@@ -6,4 +6,7 @@ export KAUST_MODULES_ROOT=$KAUST_APPS_ROOT/modules
 
 rel=$(lsb_release -d | gawk -F"\t" '{print $2}')
 os=$([[ $rel == *"Ubuntu"* ]] && echo ubuntu || echo centos)
-export  MODULEPATH=~/local/modulefiles:$KAUST_MODULES_ROOT/applications:$KAUST_MODULES_ROOT/compilers:$KAUST_MODULES_ROOT/libs:$KAUST_MODULES_ROOT/sets:$KAUST_MODULES_ROOT/$os
+# Order is important here, paths first in thew list have precedence
+MODULEPATH=~/local/modulefiles:$KAUST_MODULES_ROOT/$os:$KAUST_MODULES_ROOT/applications
+MODULEPATH=$MODULEPATH:$KAUST_MODULES_ROOT/compilers:$KAUST_MODULES_ROOT/libs:$KAUST_MODULES_ROOT/sets
+export MODULEPATH
